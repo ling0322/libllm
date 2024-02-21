a4965cacab6dfaed4729fa3f8380227ed8f6671b
Xiaoyang Chen <ling032x@gmail.com>
2024-02-21 17:31:47 +0800

    get ISA support by ruapu (#42)

---
+++ third_party/ruapu/ruapu.cc
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024 nihui (https://github.com/nihui)
+// Copyright (c) 2024 kernelbin (https://github.com/kernelbin)
+//
+// ruapu --- detect cpu isa features with single-file
+
+#define RUAPU_IMPLEMENTATION
+#include "ruapu.h"
