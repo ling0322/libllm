c25cd85d9b1936f0ba9de0bb7a7bbc970cfeb772
Xiaoyang Chen <ling032x@gmail.com>
2024-04-09 16:23:35 +0800

    add function lut::isDebug() (#62)

---
+++ src/libllm/lut/is_debug.h
@@ -0,0 +1,12 @@
+// original file: https://github.com/Tencent/ncnn/blob/056509a/src/cpu.cpp
+
+#pragma once
+
+#include <stdint.h>
+
+namespace lut {
+
+/// @brief Returns true when the process is being debugged.
+bool isDebug();
+
+}  // namespace lut
