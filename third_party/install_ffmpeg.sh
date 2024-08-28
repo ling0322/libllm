be46707e35275e31bfdceda6eae3ea35d2630fb6
Xiaoyang Chen <ling032x@gmail.com>
2024-08-28 11:34:24 +0000

    update

--- third_party/install_ffmpeg.sh
+++ third_party/install_ffmpeg.sh
@@ -46,10 +46,35 @@ echo "configuring ffmpeg ..."
     --disable-encoders \
     --enable-encoder=pcm_s16le \
     --disable-demuxers \
+    --enable-demuxer=aac \
+    --enable-demuxer=flac \
+    --enable-demuxer=flv \
+    --enable-demuxer=ogg \
+    --enable-demuxer=s16be \
+    --enable-demuxer=s16le \
+    --enable-demuxer=s32be \
+    --enable-demuxer=s32le \
+    --enable-demuxer=matroska \
     --enable-demuxer=mov \
+    --enable-demuxer=mp3 \
+    --enable-demuxer=wav \
+    --enable-demuxer=xwma \
     --disable-decoders \
     --enable-decoder=aac \
+    --enable-decoder=ac3 \
+    --enable-decoder=flac \
+    --enable-decoder=mp2 \
     --enable-decoder=mp3 \
+    --enable-decoder=opus \
+    --enable-decoder=pcm_alaw \
+    --enable-decoder=pcm_f32be \
+    --enable-decoder=pcm_f32le \
+    --enable-decoder=pcm_mulaw \
+    --enable-decoder=pcm_s16be \
+    --enable-decoder=pcm_s16le \
+    --enable-decoder=vorbis \
+    --enable-decoder=wmav1 \
+    --enable-decoder=wmav2 \
     --disable-parsers
 
 make -j
